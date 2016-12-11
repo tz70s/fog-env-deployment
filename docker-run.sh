76cdc4a633fc865cebd879eadb553f5a349fee1d
Tzu-Chiao Yeh <su3g4284zo6y7@gmail.com>
2016-12-11 15:25:59 +0800

    Update docker run

--- docker-run.sh
+++ docker-run.sh
@@ -4,5 +4,17 @@
 
 docker pull ansible/ubuntu14.04-ansible
 
-docker run --name=container1 --net=none ansible/ubuntu14.04-ansible
+docker run -itd --privileged --name=container1 --net=none ansible/ubuntu14.04-ansible
 
+# Create ovs docker connection
+# Without prec-onfigure ip address
+
+sudo ovs-docker add-port ovs-br0 eth0 container1
+
+# Then do the following actions in container
+
+docker start -i container1
+
+# mv /sbin/dhclient /usr/sbin/dhclient
+# dhclient eth0
+# ignore the error log
