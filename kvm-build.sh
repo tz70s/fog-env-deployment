83c6f23d3f75f51fb7a48eb7a693d3f016423fdb
tz70s <su3g4284zo6y7@gmail.com>
2016-12-29 17:55:38 +0800

    modify

--- kvm-build.sh
+++ kvm-build.sh
@@ -4,15 +4,48 @@
 
 # Check vm name, ovs brdige name ....
 
-virt-install \
-	--virt-type=kvm \
-	--name vm01 \
-	--ram=1024 \
-	--vcpus=1 \
-	--os-variant=auto \
-	--hvm \
-	--cdrom /home/tzuchiao/ubuntu-16.04.1-server-amd64.iso \
-	--network bridge=ovs-br0,virtualport_type='openvswitch' \
-	--graphics vnc \
-	--disk path=/var/lib/libvirt/images/vm02.img,size=20,bus=virtio \
-	
+# Check number of vm nodes
+
+if [ -z $1 ]
+then
+	echo "Please enter at least 1-8 nodes number ... exit"
+	exit 0
+else
+	if [ $1 -gt 8 ]
+	then
+		echo "Can't build > 8 nodes ... exit"
+		exit 0
+	else
+		if [ $1 -le 0 ]
+		then
+			echo "Can't build <= 0 nodes ... exit"
+			exit 0
+		fi
+		echo "$1 vms start building"
+	fi
+fi
+
+# Check image
+
+if [ -s /home/tzuchiao/ubuntu-16.04.1-server-amd64.iso ]
+then
+	echo "Check image complete"
+else
+	echo "Image file not exist ... exit"
+	exit 0
+fi
+
+for i = 1; i <= $1; i++ 
+do
+	virt-install \
+		--virt-type=kvm \
+		--name "vm0$1" \
+		--ram=2048 \
+		--vcpus=2 \
+		--os-variant=auto \
+		--hvm \
+		--cdrom /home/tzuchiao/ubuntu-16.04.1-server-amd64.iso \
+		--network bridge=ovs-br0,virtualport_type='openvswitch' \
+		--graphics vnc \
+		--disk path=/var/lib/libvirt/images/"vm0$1".img,size=20,bus=virtio \
+done
